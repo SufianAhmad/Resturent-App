836566b6f664cbd90f5f7c7bc95e516b574afde2
SufianAhmad <Sufian.Ahmad1@gmail.com>
2016-03-05 01:03:37 +0500

    Basic setup

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 class App < Sinatra::Base
+
+  enable :sessions
+  use Rack::Flash
+  
   get "/" do
     "Hello World.."
+  end
+
+  namespace "/orders" do
+    #orders
+    get do
+
+    end
+
+    #new orders placed by waiters
+    get "/new" do
+      #available dishes
+      @dishes = []
+      haml %s( orders/new )
+    end
   end
 end
