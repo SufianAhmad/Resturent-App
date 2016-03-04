c6269c0384287de280b178d8fc856d9e974f0d02
SufianAhmad <Sufian.Ahmad1@gmail.com>
2016-03-05 01:09:28 +0500

    Hound Errors

--- app.rb
+++ app.rb
@@ -1,21 +1,18 @@
 class App < Sinatra::Base
-
   enable :sessions
   use Rack::Flash
-  
   get "/" do
     "Hello World.."
   end
-
   namespace "/orders" do
-    #orders
+    # orders
     get do
 
     end
 
-    #new orders placed by waiters
+    # new orders placed by waiters
     get "/new" do
-      #available dishes
+      # available dishes
       @dishes = []
       haml %s( orders/new )
     end
